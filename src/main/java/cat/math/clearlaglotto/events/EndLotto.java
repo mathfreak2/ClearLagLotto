f0c2692eccd7cf4298b8bae21fad7eadd73a5f93
Rowan McKee <rowanmckee@gmail.com>
2019-09-17 14:18:46 -0700

    Add files via upload

--- src/main/java/cat/math/clearlaglotto/events/EndLotto.java
+++ src/main/java/cat/math/clearlaglotto/events/EndLotto.java
@@ -1,11 +1,17 @@
 package cat.math.clearlaglotto.events;
 
+import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 
+import com.earth2me.essentials.Essentials;
+
 import cat.math.clearlaglotto.ClearLagLotto;
+import cat.math.clearlaglotto.Entry;
 
 public class EndLotto extends Event {
 
@@ -16,17 +22,64 @@ public class EndLotto extends Event {
 		this.plugin = plugin;
 	}
 	
-    public void runEvent() {
+    public void runEvent(int removed) {
     	
     	Bukkit.getServer().getPluginManager().callEvent(this);
     	BeginLotto.isRunning = false;
-    	String message;
+    	String message = plugin.getLottoWinMessage();
+    	ArrayList<Entry> entries = plugin.getLottery().getEntryList();
+    	String winning_condition = plugin.getWinningCondition();
+    	int mindifference = -1;
+    	Entry winner = null;
+    	
+    	for(Entry e : entries) {
+    		
+    		if(winning_condition.equals("exact") && e.getEntry() != removed) continue;
+    		if(winning_condition.equals("blackjack") && e.getEntry() > removed) continue;
+    		int difference = Math.abs(e.getEntry()-removed);
+    		if(mindifference == -1) {
+    			mindifference = difference;
+    			winner = e;
+    		}
+    		else {
+    			if(difference < mindifference) {
+    				mindifference = difference;
+    				winner = e;
+    			}
+    			else if(difference == mindifference) {
+    				if(e.getEntryTime() < winner.getEntryTime()) {
+    					winner = e;
+    				}
+    			}
+    		}
+    	}
+    	
+    	if(winner == null) Bukkit.getServer().broadcastMessage(plugin.getLottoNoWinMessage());
+    	else {
+    		
+    		double winnings = plugin.getLottery().getPot();
+    		if(winner.getEntry() == removed) {
+    			winnings *= plugin.getJackpotMultiplier();
+    			message = plugin.getLottoJackpotMessage();
+    		}
+    		
+    		message.replaceAll("+name",winner.getPlayer().getName());
+    		message.replaceAll("+guess", ""+winner.getEntry());
+    		message.replaceAll("+winnings", ""+winnings);
+    		Bukkit.getServer().broadcastMessage(message);
+    		
+    		Essentials essentials = plugin.getEssentials();
+    		CommandSender sender = (CommandSender)Bukkit.getServer().getConsoleSender();
+    		Bukkit.getServer().dispatchCommand(sender, "eco give "+winner.getPlayer().getName()+" "+winnings);
+    		plugin.getLottery().setPot(0);
+    		
+    	}
     }
 	
 	@Override
 	public HandlerList getHandlers() {
 		// TODO Auto-generated method stub
-		return null;
+		return handlers;
 	}
 
 }
